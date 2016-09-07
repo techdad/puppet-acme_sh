586504b7d487636540d3bc9acc26c66e374b2240
Walter Kleynscheldt <walter.kleynscheldt@hetzner.co.za>
2016-09-07 15:21:19 +0200

    Properly parameterise module

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,12 +6,18 @@
 # Parameters
 # ----------
 #
+# * `$acme_repo_path`
+# The full path to clone the acme.sh git repository in order to install from.
+#
 # * `$acme_home`
 # The full path to install the acme.sh script into (overriding $HOME/.acme.sh).
 #
 # * `$acme_accountemail`
 # Specify the email address to be used in Letsencrypt API communications.
 # This email address will later receive certificate expiry warnings.
+#
+# * `$manage_dependencies`
+# Boolean to specify whether or not to install the required package dependencies
 #
 # Authors
 # -------
@@ -23,44 +29,33 @@
 #
 # Apache 2.0
 #
-class acme_sh {
+class acme_sh (
+  $acme_repo_path      = $acme_sh::params::acme_repo_path,
+  $acme_home           = $acme_sh::params::acme_home,
+  $acme_accountemail   = $acme_sh::params::acme_accountemail,
+  $manage_dependencies = $acme_sh::params::manage_dependencies,
+  ) inherits acme_sh::params {
 
-  # install packages that are dependencies
-  if ! defined(Package['git']) {
-    package { 'git':
-      ensure => present,
-    }
+  validate_string($acme_home, $acme_accountemail)
+  validate_bool($manage_dependencies)
+
+  if $manage_dependencies {
+    $dependencies = ['git']
+    ensure_packages($dependencies)
+    Package[$dependencies] -> Vcsrepo[$acme_home]
   }
 
-  if ! defined(Package['nc']) {
-    package { 'nc':
-      ensure => present,
-    }
+  vcsrepo {'$acme_repo':
+    ensure   => latest,
+    provider => git,
+    source   => 'https://github.com/Neilpang/acme.sh.git',
+    notify   => Exec['acme_sh::self-install'],
   }
 
-  # clone acme.sh from git if not there at all
-  exec { 'acme_sh::git clone':
-    creates => '/tmp/.acme.sh',
-    command => 'git clone https://github.com/Neilpang/acme.sh.git /tmp/.acme.sh || (rmdir /tmp/.acme.sh && exit 1)',
-    path    => ['/bin', '/usr/bin'],
-    require => Package['git'],
-  }
-
-  # update (pull) from git if already there
-  exec { 'acme_sh::git pull':
-    onlyif  => 'test -f /tmp/.acme.sh/acme.sh',
-    command => 'git pull --rebase --stat origin master',
-    path    => ['/bin', '/usr/bin'],
-    cwd     => '/tmp/.acme.sh',
-    require => Package['git'],
-    notify  => Exec['acme_sh::self-install'],
-  }
-
-  # install/upgrade the script from the git source
   exec { 'acme_sh::self-install':
-    command     => "/bin/sh ./acme.sh --install  --home ${acme_home} --accountemail \"${acme_accountemail}\"",
+    command     => "/bin/sh ./acme.sh --install --home ${acme_home} --accountemail \"${acme_accountemail}\"",
     path        => ['/bin', '/usr/bin'],
-    cwd         => '/tmp/.acme.sh',
+    cwd         => $acme_,
     refreshonly => true,
   }
 
---
+++ manifests/params.pp
@@ -0,0 +1,7 @@
+class acme_sh::params {
+  $acme_repo_path      = '/tmp/acme.sh'
+  $acme_home           = '/opt/acme/'
+  $acme_accountemail   = undef
+  $manage_dependencies = false
+}
+
